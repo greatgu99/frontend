9be0c7c74ae3cd3777c10940d9b8d9659a5c8234
GreatGu <guyuhan1999@126.com>
2022-04-12 21:50:14 +0800

    change base_url

--- src/util/request.js
+++ src/util/request.js
@@ -5,7 +5,7 @@ import axios from "axios";
 // import { getCookie } from "../util/api";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1/api",
+  baseURL: "http://124.220.22.44/api",
   timeout: 100000
 });
 /**
